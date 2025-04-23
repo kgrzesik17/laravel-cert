06e4d1cbac7f0d4b9aada5782e20035414e5ccad
Kacper <kgrzesik17@gmail.com>
2025-04-23 19:54:13 +0200

    poly retrieving

--- cms/routes/web.php
+++ cms/routes/web.php
@@ -296,10 +296,23 @@ use App\Models\Photo;
 // });
 
 
-Route::get('/photo/{id}/post', function($id){
+// Route::get('/photo/{id}/post', function($id){
 
-   $photo = Photo::findOrFail($id);
+//    $photo = Photo::findOrFail($id);
 
-   return $photo->imageable;
+//    return $photo->imageable;
+
+// });
+
+
+// Polymorphic Many to Many
+
+Route::get('/post/tag', function() {
+
+   $post = Post::find(1);
+
+   foreach($post->tags as $tag) {
+      echo $tag->name;
+   }
 
 });
