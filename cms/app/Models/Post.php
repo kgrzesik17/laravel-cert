213ffa679d61d24398414397c71d19095db7250d
Kacper <kgrzesik17@gmail.com>
2025-04-23 19:38:34 +0200

    polymorphic relation many to many

--- cms/app/Models/Post.php
+++ cms/app/Models/Post.php
@@ -23,6 +23,10 @@ class Post extends Model
         return $this->morphMany('App\Models\Photo', 'imageable');
     }
 
+    public function tags() {
+        return $this->morphToMany('App\Models\Tag', 'taggable');
+    }
+
     //
     // protected $table = 'posts';
     // protected $primaryKey = 'posts_id';
